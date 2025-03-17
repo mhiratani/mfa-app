77e4305aec9d41218a00f9e260cc17835f542bd9
MasayukiHiratani <m.malacca@outlook.com>
2025-03-17 14:05:10 +0900

    # Release version 1.0.2 - エクスポート/インポート機能のセキュリティとUX向上

--- copy_and_sign.ps1
+++ copy_and_sign.ps1
@@ -1,7 +1,7 @@
 # copy_and_sign.ps1
 $appName = "MagicalFlyingAlpaca"
 $tempName = "TempBuildApp"
-$version = "1.0.1"
+$version = "1.0.2"
 
 # Check if temporary file exists
 if (-not (Test-Path "dist\$tempName.exe")) {
